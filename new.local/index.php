f9e8bd5f869777e0871a6e8f9b44ac40c0a262be
Kyrchanov <a.kyrchanov1990@gmail.com>
2017-09-05 15:25:18 +0300

    Add files via upload

---
+++ new.local/index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tank App</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="js/slick-1.6.0/slick/slick.css">
+      <link rel="stylesheet" type="text/css" href="js/slick-1.6.0/slick/slick-theme.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+     <script src="js/slick-1.6.0/slick/slick.js" type="text/javascript" charset="utf-8"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+
+
+<body>
+
+<header>
+    <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-2">
+              <img src="img/logo.png" alt="">
+          </div>
+          <div class="col-lg-5 col-lg-offset-5">
+            <div class="navi">
+                 <ul class="nav nav-pills">
+                      <li><a href="#">How it works</a></li>
+                      <li><a href="#">About us</a></li>
+                      <li><a href="#">Contact us</a></li>
+                      <li><a href="#">Blog</a></li>
+                 </ul>
+              </div>
+          </div>
+        </div>
+    </div>
+</header>
+
+<div class="container-fluid" id="bg-banner">
+  <div class="row">
+     <div class="col-lg-7">
+        <div class="wrap">
+            <h1>
+               TRACK YOUR NEXT <br> FUEL SHOPS
+            </h1>
+           <div class="text-one">
+               <p>Monitor and Alert on all petrol bunks near to you </p>
+           </div>
+           <div class="text-two">
+               <p>Signup For</p>
+           </div>
+             
+    
+        <button>FREE</button>
+
+   
+               
+        </div>
+
+     </div>
+      
+  </div>
+   
+   
+    <div class="row">
+        <div class="col-lg-3 col-lg-offset-3">
+            <div class="finde">
+                 <input type="search" placeholder="Your e-mail adress" name="go" />
+                    <input type="submit" value="SIGNUP" />
+            </div>
+        </div>
+        
+        <div class="col-lg-6">
+           <div class="hand">
+                <img src="img/Hand.png" alt="">
+            </div>
+        </div>
+        
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-carusel">
+                <div><img src="img/Evernote.png" alt=""></div>
+                <div><img src="img/Firefox.png" alt=""></div>
+                <div><img src="img/Google.png" alt=""></div>
+                <div><img src="img/Microsoft.png" alt=""></div>
+                <div><img src="img/Sony.png" alt=""></div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid" id="app">
+    <div class="row">
+        <div class="col-lg12 text-center">
+            <h1><b>WHY TANK APP?</b></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="row item">
+                <div class="col-lg-12">
+                    <h1><div class="wrap"><img src="img/ok.png" alt=""></div> Reliable and Secure Platform </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, provident, dolore. Sit earum voluptas eaque recusandae voluptates repudiandae quos maxime, natus consequuntur doloribus accusantium enim temporibus porro rerum sequi expedita!</p>
+                </div>
+            </div>
+                 <div class="row item">
+                <div class="col-lg-12">
+                    <h1><div class="wrap"><img src="img/clock.png" alt=""></div>  Everything is perfectly orgainized </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, provident, dolore. Sit earum voluptas eaque recusandae voluptates repudiandae quos maxime, natus consequuntur doloribus accusantium enim temporibus porro rerum sequi expedita!</p>
+                </div>
+            </div>
+            <div class="row item">
+                <div class="col-lg-12">
+                    <h1><div class="wrap"><img src="img/tweet.png" alt=""></div>  Rapid customer support </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, provident, dolore. Sit earum voluptas eaque recusandae voluptates repudiandae quos maxime, natus consequuntur doloribus accusantium enim temporibus porro rerum sequi expedita!</p>
+                </div>
+            </div>
+                 <div class="row item">
+                <div class="col-lg-12">
+                    <h1><div class="wrap"><img src="img/organ.png" alt=""> </div> Tons of features and easy to use  </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, provident, dolore. Sit earum voluptas eaque recusandae voluptates repudiandae quos maxime, natus consequuntur doloribus accusantium enim temporibus porro rerum sequi expedita!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6" id="map">
+            <img src="img/macbook.png" alt="" id="mb" class="pull-right">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d380059.5561632262!2d12.255814103766959!3d41.90973055559072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f6196f9928ebb%3A0xb90f770693656e38!2sRom%2C+Metropolitan+City+of+Rome%2C+Italy!5e0!3m2!1sde!2sua!4v1501142222407" width="540" height="445" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+   
+</div>  
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-12">
+   
+    <div class="container-slider">
+        <div class="bgray" id="slide">
+          <div class="col-lg-12">
+              <div class="main"><h1><b>WHY TANK APP DIFFRENT?</b></h1></div>
+          </div> 
+           <div class="col-lg-6">
+            <div class="info">
+              <h1><div class="icon"><img src="img/skys.png" alt=""> </div> Works on any device anyware</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, provident, dolore. Sit earum voluptas eaque recusandae voluptates repudiandae quos maxime, natus consequuntur doloribus accusantium enim temporibus porro rerum sequi expedita!</p>
+             </div>
+           </div>
+            <div class="col-lg-6">
+               <div class="ship">
+                   <img src="img/ships.png" alt="">
+               </div>
+                
+            </div>
+        </div>
+        <div><img src="img/swiss-lpg-map-new.png" alt=""></div>
+        <div><img src="img/shell-service-station.png" alt=""></div>
+    </div>
+</div>
+</div>
+</div>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-lg-12">
+        <div class="col-lg12 text-center">
+        <h1><b>SAVE TIME & GAIN AGILITY</b></h1>
+    </div>
+    </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="col-lg12 text-small">
+            <p><dfn>Using our cloud Monitoring Services So you can Focous on Runing</dfn></p>
+        </div>
+    </div>
+<div class="col-lg-12 mail">
+  <input type="find" placeholder="Enter Your E-mail Adress" name="go" />
+    <input type="button" value="SIGN UP" />  
+</div>
+
+</div>
+
+
+
+<div class="container-fluid" id="bg-footer">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="footer">
+           <div class="col-lg-2">
+               <img src="img/logotank.png" alt="">
+           </div>
+            
+             <div class="col-lg-5 col-lg-offset-3">
+            <div class="navi">
+                 <ul class="nav nav-pills">
+                      <li><a href="#">How it works</a></li>
+                      <li><a href="#">About us</a></li>
+                      <li><a href="#">Contact us</a></li>
+                      <li><a href="#">Blog</a></li>
+                 </ul>
+              </div>
+          </div>
+          <div class="col-lg-2">
+              <img src="img/iconi.png" alt="">
+          </div>
+          </div>
+        </div>
+    </div>
+</div>
+
+<script>
+$('.container-carusel').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+});    
+    $('.container-slider').slick();
+</script>
+
+</body>
+
+</html>
